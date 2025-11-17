04545c1fe4e45dcdeb3664cf43beba69d6eb6d49
Julien5 <julien_bourgeois@yahoo.fr>
2025-11-17 16:28:09 +0100

    bind MapRenderer model with Provider and display a map

--- frontend/ui/lib/src/screens/interactive/interactive_screen.dart
+++ frontend/ui/lib/src/screens/interactive/interactive_screen.dart
@@ -1,18 +1,19 @@
 //import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:ui/src/models/futurerenderer.dart';
 import 'package:ui/src/models/root.dart';
+import 'package:ui/src/rust/api/bridge.dart';
+import 'package:ui/src/screens/segments/future_rendering_widget.dart';
 
 class InteractiveMapView extends StatelessWidget {
   const InteractiveMapView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    RootModel rootModel = Provider.of<RootModel>(context);
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Text("hi");
+    return Consumer<MapRenderer>(
+      builder: (context, mapRenderer, child) {
+        return FutureRenderingWidget(future: mapRenderer);
       },
     );
   }
@@ -36,9 +37,14 @@ class InteractiveScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext ctx) {
+    RootModel root = Provider.of<RootModel>(ctx);
+    Segment trackSegment = root.trackSegment();
     return Scaffold(
       appBar: AppBar(title: const Text('Map')),
-      body: InteractiveConsumer(),
+      body: ChangeNotifierProvider<MapRenderer>(
+        create: (_) => MapRenderer(root.getBridge(), trackSegment),
+        child: InteractiveConsumer(),
+      ),
     );
   }
 }
