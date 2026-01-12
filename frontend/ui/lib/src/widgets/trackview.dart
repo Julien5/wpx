906bea19f7d1278e31b943c8299e46715aec7dbb
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2026-01-12 22:37:09 +0100

    move TrackView to a separate file

---
+++ frontend/ui/lib/src/widgets/trackview.dart
@@ -0,0 +1,116 @@
+import 'dart:developer' as developer;
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ui/src/models/futurerenderer.dart';
+import 'package:ui/src/models/segmentmodel.dart';
+import 'package:ui/src/rust/api/bridge.dart';
+import 'package:ui/src/widgets/future_rendering_widget.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+
+class RendererParameters {
+  final Set<InputType> kinds;
+  final TrackData trackData;
+  const RendererParameters({required this.kinds, required this.trackData});
+  ValueKey createKey() {
+    final sortedKinds = kinds.map((k) => k.toString()).toList()..sort();
+    return ValueKey('${trackData.toString()}|${sortedKinds.join(",")}');
+  }
+}
+
+class TrackView extends StatefulWidget {
+  final RendererParameters parameters;
+  const TrackView({super.key, required this.parameters});
+
+  static TrackView make(Set<InputType> kinds, TrackData trackData) {
+    RendererParameters p = RendererParameters(
+      kinds: kinds,
+      trackData: trackData,
+    );
+    return TrackView(key: p.createKey(), parameters: p);
+  }
+
+  @override
+  State<TrackView> createState() => _TrackViewState();
+}
+
+class _TrackViewState extends State<TrackView> {
+  TrackData current = TrackData.wheel;
+  VisibilityInfo? visibilityInfo;
+  FutureRenderer? futureRenderer;
+
+  // The argument type is BuildContext, but using it yields
+  // a crash. Dont ask me why.
+  FutureRenderer _createRenderer(_) {
+    SegmentModel model = Provider.of<SegmentModel>(context);
+    futureRenderer = model.makeRenderer(
+      widget.parameters.kinds,
+      widget.parameters.trackData,
+    );
+    return futureRenderer!;
+  }
+
+  FutureRenderer _onSegmentModelChanged(FutureRenderer? renderer) {
+    developer.log("update future");
+    renderer!.reset();
+    startRendererIfNeeded();
+    return renderer;
+  }
+
+  void _onVisibilityChanged(VisibilityInfo info) {
+    if (!mounted) {
+      return;
+    }
+    visibilityInfo = info;
+
+    if (visibilityInfo!.visibleFraction == 0) {
+      return;
+    }
+    if (futureRenderer == null) {
+      return;
+    }
+    futureRenderer!.setSize(visibilityInfo!.size);
+    startRendererIfNeeded();
+  }
+
+  // takes visibility and renderer dirtyness into account.
+  void startRendererIfNeeded() {
+    if (futureRenderer == null) {
+      return;
+    }
+    bool needed =
+        visibilityInfo != null &&
+        visibilityInfo!.visibleFraction > 0 &&
+        futureRenderer!.needsStart();
+    if (needed) {
+      futureRenderer!.start();
+    }
+  }
+
+  @override
+  Widget build(BuildContext ctx) {
+    // reacts on change in the segmentmodel..
+    SegmentModel model = Provider.of<SegmentModel>(ctx);
+    Widget innerWidget = LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return VisibilityDetector(
+          key: widget.parameters.createKey(),
+          onVisibilityChanged: _onVisibilityChanged,
+          child: FutureRenderingWidget(interactive: false),
+        );
+      },
+    );
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: model),
+        ChangeNotifierProxyProvider<SegmentModel, FutureRenderer>(
+          create: _createRenderer,
+          update: (context, segment, futureRenderer) {
+            return _onSegmentModelChanged(futureRenderer);
+          },
+        ),
+      ],
+      child: innerWidget,
+    );
+  }
+}
