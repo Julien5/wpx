0255a008ce3ee235a1775230c04c8182569f71c8
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2026-01-15 23:26:34 +0100

    fix zip generation on wasm

--- backend/src/zipexport.rs
+++ backend/src/zipexport.rs
@@ -1,12 +1,18 @@
 #![allow(non_snake_case)]
 use std::io::{Cursor, Write};
-use zip::write::{FileOptions, ZipWriter}; // Add Write trait
+use zip::write::{SimpleFileOptions, ZipWriter};
+use zip::CompressionMethod; // Add Write trait
 
 pub fn generate(gpx: &[u8], pdf: &[u8]) -> Vec<u8> {
     let buffer = Cursor::new(Vec::new());
     let mut zip = ZipWriter::new(buffer);
 
-    let options = FileOptions::<()>::default().compression_method(zip::CompressionMethod::Deflated);
+    //let options = FileOptions::<()>::default().compression_method(zip::CompressionMethod::Deflated);
+
+    // Use a fixed timestamp to avoid system time calls on wasm
+    let options = SimpleFileOptions::default()
+        .compression_method(CompressionMethod::Stored)
+        .last_modified_time(zip::DateTime::from_date_and_time(2024, 1, 1, 0, 0, 0).unwrap());
 
     // Add GPX file
     zip.start_file("waypoints.gpx", options).unwrap();
