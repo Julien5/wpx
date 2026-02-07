d54db6493783cca3eacfaca9b8658cf89dd309e2
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2026-02-07 20:45:26 +0100

    fix update of the screen configuration
    - exclude update from ScreenShells that are on the navigation stack
    but off-screen.

--- frontend/ui/lib/src/screens/shell/screen_shell.dart
+++ frontend/ui/lib/src/screens/shell/screen_shell.dart
@@ -11,21 +11,40 @@ class ScreenShell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ScreenConfiguration screen = Provider.of<ScreenConfiguration>(context);
     return LayoutBuilder(
       builder: (context, constraints) {
-        Future.microtask(() {
-          if (!context.mounted) return;
-          context.read<ScreenConfiguration>().updateConstraints(constraints);
-        });
-
         final currentLocation = GoRouterState.of(context).matchedLocation;
+        final router = GoRouter.of(context);
+        final isTopRoute =
+            router
+                .routerDelegate
+                .currentConfiguration
+                .matches
+                .last
+                .matchedLocation ==
+            currentLocation;
+
+        debugPrint(
+          "CONST: $constraints, isTop: $isTopRoute, path: $currentLocation",
+        );
+
+        // Only update if this is the visible (top) route
+        if (isTopRoute) {
+          Future.microtask(() {
+            if (!context.mounted) return;
+            context.read<ScreenConfiguration>().updateConstraints(constraints);
+          });
+        }
         FociModel model = Provider.of<FociModel>(context, listen: false);
         model.load(currentLocation);
+        ScreenConfiguration screen = Provider.of<ScreenConfiguration>(
+          context,
+          listen: false,
+        );
         if (screen.isMobile()) {
           return MobileShell();
         }
-        return DesktopShell();
+        return MobileShell();
       },
     );
   }
