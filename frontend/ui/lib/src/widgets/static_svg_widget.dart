cdf15b347bfc96317d7b0918634a02c29acab813
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2026-03-05 07:07:48 +0100

    apply an offset in static svg widget to center the content

--- frontend/ui/lib/src/widgets/static_svg_widget.dart
+++ frontend/ui/lib/src/widgets/static_svg_widget.dart
@@ -1,4 +1,3 @@
-import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 import 'package:ui/src/svgelements.dart';
 import 'package:ui/utils.dart';
@@ -15,15 +14,21 @@ class StaticSvgWidget extends StatelessWidget {
         Size displaySize = constraints.biggest;
         double scale = scaleDown(svgRootElement.size, displaySize);
         Size scaledSize = svgRootElement.size * scale;
-        developer.log(
-          "objectSize=${svgRootElement.size}, constraints-size=$displaySize => scale=$scale",
+        Offset offset = Offset(
+          0.5 * (displaySize.width - scaledSize.width),
+          0.5 * (displaySize.height - scaledSize.height),
         );
+        debugPrint("object=${svgRootElement.size}");
+        debugPrint("display=$displaySize");
+        debugPrint("scaled=$scaledSize");
+        debugPrint("=> scale=$scale, offset=$offset");
         return ClipRect(
           child: CustomPaint(
             size: scaledSize,
             painter: StaticSvgPainter(
               root: svgRootElement,
               renderingScale: scale,
+              offset: offset,
             ),
           ),
         );
@@ -35,12 +40,29 @@ class StaticSvgWidget extends StatelessWidget {
 class StaticSvgPainter extends CustomPainter {
   final SvgRootElement root;
   final double renderingScale;
+  final Offset offset;
 
-  StaticSvgPainter({required this.root, required this.renderingScale});
+  StaticSvgPainter({
+    required this.root,
+    required this.renderingScale,
+    required this.offset,
+  });
 
   @override
   void paint(Canvas canvas, Size drawArea) {
+    /*final Paint framePaint =
+        Paint()
+          ..color = Colors.red
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 10.0;
+    canvas.drawRect(Offset.zero & drawArea, framePaint);
+    */
+
+    // i wonder if that order is correct, but the content
+    // is properly centered...
+    canvas.translate(offset.dx, offset.dy);
     canvas.scale(renderingScale);
+
     Sheet sheet = Sheet(
       canvas: canvas,
       size: drawArea,
